3ca54d65af6769c0fffcf633bae4b46892d355c0
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-09-16 18:13:37 -0500

    Solution for Two Integer Sum II

--- Two Pointers/twoIntegerSumII.py
+++ Two Pointers/twoIntegerSumII.py
@@ -1,6 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
-            if target - numbers[i] != numbers[i]:
+            if target - numbers[i] == numbers[i]:
+                if numbers.count(target - numbers[i]) > 1:
+                    a = numbers[i]
+                    numbers[i] = -10000
+                    if target - a in numbers:
+                        return [i + 1, numbers.index(target - a) + 1]
+
+            else:
                 if target - numbers[i] in numbers:
                     return [i + 1, numbers.index(target - numbers[i]) + 1]
