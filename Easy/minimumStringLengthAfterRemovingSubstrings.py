b750de049bcb1c2e061c16406ce78ed44eaafe9e
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-10-07 18:28:21 -0500

    Stack Solution for mimimum string length after removing substrings

--- Easy/minimumStringLengthAfterRemovingSubstrings.py
+++ Easy/minimumStringLengthAfterRemovingSubstrings.py
@@ -1,7 +1,27 @@
+# class Solution:
+#     def minLength(self, s: str) -> int:
+#         if "AB" not in s and "CD" not in s:
+#             return len(s)
+
+#         s = s.replace("AB", "").replace("CD", "")
+#         return self.minLength(s)
+
+
 class Solution:
     def minLength(self, s: str) -> int:
-        if "AB" not in s and "CD" not in s:
-            return len(s)
+        stack = []
 
-        s = s.replace("AB", "").replace("CD", "")
-        return self.minLength(s)
+        for i in range(len(s)):
+            if len(stack) == 0:
+                stack.append(s[i])
+                continue
+            if s[i] == "B" and stack[-1] == "A":
+                stack.pop()
+
+            elif s[i] == "D" and stack[-1] == "C":
+                stack.pop()
+
+            else:
+                stack.append(s[i])
+
+        return len(stack)
